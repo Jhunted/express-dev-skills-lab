2566186c946788cebc1fc10fa44885b9b2ae8643
Joseph Hunt <jhunted54@gmail.com>
2019-09-09 08:13:08 -0500

    adds express generator, models, controller, routes, and views

---
+++ models/skill.js
@@ -0,0 +1,28 @@
+const skills = [
+    {skill: 'Javascript', done: true},
+    {skill: 'HTML', done: false},
+    {skill: 'CSS', done: false}
+  ];
+  
+  module.exports = {
+    getAll,
+    getOne,
+    create,
+    deleteOne
+  };
+
+  function deleteOne(id) {
+      skills.splice(id, 1);
+  }
+
+  function create(skill) {
+      skills.push(skill);
+  }
+
+  function getOne(id) {
+      return skills[id];
+  }
+  
+  function getAll() {
+    return skills;
+  }
